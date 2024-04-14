131848bae67fbc52b97844c73607d179f98982c1
Kamil <34790800+Tiviacz1337@users.noreply.github.com>
2024-04-15 00:16:38 +0200

    🔧 Reorganized creative tab

--- src/main/java/com/tiviacz/pizzacraft/init/ModCreativeTabs.java
+++ src/main/java/com/tiviacz/pizzacraft/init/ModCreativeTabs.java
@@ -23,39 +23,60 @@ public class ModCreativeTabs
         ItemStack stack = ModItems.PIZZA_SLICE.get().getDefaultInstance();
         ItemStackHandler handler = new ItemStackHandler(6);
         handler.setStackInSlot(0, ModItems.CHEESE.get().getDefaultInstance());
-        handler.setStackInSlot(1, ModItems.PEPPER_SLICE.get().getDefaultInstance());
-        handler.setStackInSlot(2, ModItems.WING.get().getDefaultInstance());
-        handler.setStackInSlot(3, ModItems.BROCCOLI.get().getDefaultInstance());
-        handler.setStackInSlot(4, ModItems.ONION_SLICE.get().getDefaultInstance());
-        handler.setStackInSlot(5, ModItems.TOMATO_SLICE.get().getDefaultInstance());
+        //handler.setStackInSlot(1, ModItems.PEPPER_SLICE.get().getDefaultInstance());
+        //handler.setStackInSlot(2, ModItems.WING.get().getDefaultInstance());
+        //handler.setStackInSlot(3, ModItems.BROCCOLI.get().getDefaultInstance());
+        //handler.setStackInSlot(4, ModItems.ONION_SLICE.get().getDefaultInstance());
+        //handler.setStackInSlot(5, ModItems.TOMATO_SLICE.get().getDefaultInstance());
         NBTUtils.saveInventoryToStack(stack, handler);
         return stack;
     }
 
     public static void displayItems(CreativeModeTab.ItemDisplayParameters displayParameters, CreativeModeTab.Output output)
     {
-        output.accept(ModBlocks.PIZZA.get());
-        output.accept(ModBlocks.RAW_PIZZA.get());
-
-        output.accept(ModBlocks.DOUGH.get());
         output.accept(ModBlocks.PIZZA_STATION.get());
         output.accept(ModBlocks.OVEN.get());
-
-        output.accept(ModBlocks.OAK_CHOPPING_BOARD.get());
-        output.accept(ModBlocks.BIRCH_CHOPPING_BOARD.get());
-        output.accept(ModBlocks.SPRUCE_CHOPPING_BOARD.get());
-        output.accept(ModBlocks.JUNGLE_CHOPPING_BOARD.get());
-        output.accept(ModBlocks.ACACIA_CHOPPING_BOARD.get());
-        output.accept(ModBlocks.DARK_OAK_CHOPPING_BOARD.get());
-        output.accept(ModBlocks.CRIMSON_CHOPPING_BOARD.get());
-        output.accept(ModBlocks.WARPED_CHOPPING_BOARD.get());
         output.accept(ModBlocks.OLIVE_CHOPPING_BOARD.get());
-
-        output.accept(ModBlocks.GRANITE_BASIN.get());
         output.accept(ModBlocks.DIORITE_BASIN.get());
-        output.accept(ModBlocks.ANDESITE_BASIN.get());
-        output.accept(ModBlocks.BASALT_BASIN.get());
-        output.accept(ModBlocks.BLACKSTONE_BASIN.get());
+
+        output.accept(ModBlocks.DOUGH.get());
+        output.accept(ModBlocks.RAW_PIZZA.get());
+        output.accept(ModBlocks.PIZZA.get());
+
+        //output.accept(ModBlocks.OAK_CHOPPING_BOARD.get());
+        //output.accept(ModBlocks.BIRCH_CHOPPING_BOARD.get());
+        //output.accept(ModBlocks.SPRUCE_CHOPPING_BOARD.get());
+        //output.accept(ModBlocks.JUNGLE_CHOPPING_BOARD.get());
+        //output.accept(ModBlocks.ACACIA_CHOPPING_BOARD.get());
+        //output.accept(ModBlocks.DARK_OAK_CHOPPING_BOARD.get());
+        //output.accept(ModBlocks.CRIMSON_CHOPPING_BOARD.get());
+        //output.accept(ModBlocks.WARPED_CHOPPING_BOARD.get());
+        //output.accept(ModBlocks.OLIVE_CHOPPING_BOARD.get());
+
+        //output.accept(ModBlocks.GRANITE_BASIN.get());
+        //output.accept(ModBlocks.DIORITE_BASIN.get());
+        //output.accept(ModBlocks.ANDESITE_BASIN.get());
+        //output.accept(ModBlocks.BASALT_BASIN.get());
+        //output.accept(ModBlocks.BLACKSTONE_BASIN.get());
+
+        output.accept(ModBlocks.OLIVE_LOG.get());
+        output.accept(ModBlocks.OLIVE_WOOD.get());
+        output.accept(ModBlocks.STRIPPED_OLIVE_LOG.get());
+        output.accept(ModBlocks.STRIPPED_OLIVE_WOOD.get());
+        output.accept(ModBlocks.OLIVE_PLANKS.get());
+        output.accept(ModBlocks.OLIVE_STAIRS.get());
+        output.accept(ModBlocks.OLIVE_SLAB.get());
+        output.accept(ModBlocks.OLIVE_FENCE.get());
+        output.accept(ModBlocks.OLIVE_FENCE_GATE.get());
+        output.accept(ModBlocks.OLIVE_DOOR.get());
+        output.accept(ModBlocks.OLIVE_TRAPDOOR.get());
+        output.accept(ModBlocks.OLIVE_PRESSURE_PLATE.get());
+        output.accept(ModBlocks.OLIVE_BUTTON.get());
+        output.accept(ModBlocks.OLIVE_BOOKSHELF.get());
+
+        output.accept(ModBlocks.OLIVE_LEAVES.get());
+        output.accept(ModBlocks.FRUIT_OLIVE_LEAVES.get());
+        output.accept(ModBlocks.OLIVE_SAPLING.get());
 
         output.accept(ModItems.CHEF_HAT.get());
         output.accept(ModItems.CHEF_SHIRT.get());
@@ -67,8 +88,6 @@ public class ModCreativeTabs
         output.accept(ModItems.PIZZA_DELIVERY_LEGGINGS.get());
         output.accept(ModItems.PIZZA_DELIVERY_BOOTS.get());
         output.accept(ModBlocks.RED_PIZZA_BAG.get());
-
-        output.accept(ModItems.ROLLING_PIN.get());
 
         output.accept(ModItems.STONE_KNIFE.get());
         output.accept(ModItems.GOLDEN_KNIFE.get());
@@ -82,43 +101,27 @@ public class ModCreativeTabs
         output.accept(ModItems.DIAMOND_PIZZA_PEEL.get());
         output.accept(ModItems.NETHERITE_PIZZA_PEEL.get());
 
+        output.accept(ModItems.ROLLING_PIN.get());
+
         output.accept(ModItems.PIZZA_SLICE.get());
-        output.accept(ModItems.OLIVE_OIL.get());
-        output.accept(ModItems.TOMATO_SAUCE.get());
-        output.accept(ModItems.HOT_SAUCE.get());
 
-        output.accept(ModBlocks.OLIVE_PLANKS.get());
-        output.accept(ModBlocks.OLIVE_SAPLING.get());
-        output.accept(ModBlocks.OLIVE_LOG.get());
-        output.accept(ModBlocks.STRIPPED_OLIVE_LOG.get());
-        output.accept(ModBlocks.STRIPPED_OLIVE_WOOD.get());
-        output.accept(ModBlocks.OLIVE_WOOD.get());
-        output.accept(ModBlocks.OLIVE_LEAVES.get());
-        output.accept(ModBlocks.FRUIT_OLIVE_LEAVES.get());
-        output.accept(ModBlocks.OLIVE_SLAB.get());
-        output.accept(ModBlocks.OLIVE_PRESSURE_PLATE.get());
-        output.accept(ModBlocks.OLIVE_FENCE.get());
-        output.accept(ModBlocks.OLIVE_TRAPDOOR.get());
-        output.accept(ModBlocks.OLIVE_FENCE_GATE.get());
-        output.accept(ModBlocks.OLIVE_BUTTON.get());
-        output.accept(ModBlocks.OLIVE_STAIRS.get());
-        output.accept(ModBlocks.OLIVE_DOOR.get());
-        output.accept(ModBlocks.OLIVE_BOOKSHELF.get());
+        output.accept(ModBlocks.CHEESE_BLOCK.get());
+        output.accept(ModItems.CHEESE.get());
 
+        output.accept(ModItems.TOMATO.get());
+        output.accept(ModItems.ONION.get());
+        output.accept(ModItems.CUCUMBER.get());
+        output.accept(ModItems.PEPPER.get());
         output.accept(ModItems.BROCCOLI.get());
         output.accept(ModItems.CORN.get());
-        output.accept(ModItems.CUCUMBER.get());
-        output.accept(ModItems.ONION.get());
-        output.accept(ModItems.PEPPER.get());
-        output.accept(ModItems.TOMATO.get());
         output.accept(ModItems.PINEAPPLE.get());
         output.accept(ModItems.OLIVE.get());
 
-        output.accept(ModItems.CUCUMBER_SLICE.get());
+        output.accept(ModItems.TOMATO_SLICE.get());
         output.accept(ModItems.ONION_SLICE.get());
+        output.accept(ModItems.CUCUMBER_SLICE.get());
         output.accept(ModItems.PEPPER_SLICE.get());
         output.accept(ModItems.PINEAPPLE_SLICE.get());
-        output.accept(ModItems.TOMATO_SLICE.get());
         output.accept(ModItems.MUSHROOM_SLICE.get());
         output.accept(ModItems.HAM.get());
         output.accept(ModItems.WING.get());
@@ -126,8 +129,10 @@ public class ModCreativeTabs
         output.accept(ModItems.FLOUR.get());
         output.accept(ModItems.CORN_FLOUR.get());
 
-        output.accept(ModBlocks.CHEESE_BLOCK.get());
-        output.accept(ModItems.CHEESE.get());
+        output.accept(ModItems.TOMATO_SAUCE.get());
+        output.accept(ModItems.HOT_SAUCE.get());
+        output.accept(ModItems.OLIVE_OIL.get());
+
         output.accept(ModItems.BROCCOLI_SEEDS.get());
         output.accept(ModItems.CUCUMBER_SEEDS.get());
         output.accept(ModItems.PEPPER_SEEDS.get());
